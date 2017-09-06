7f33ef4789e8f3fe8010b62107fb407325265e01
Rahul Daware <rahuldaware19@gmail.com>
2017-09-06 00:07:10 -0700

    Added all API functions

--- readable/src/App.js
+++ readable/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Post from './components/Post.js';
+import * as ReadableAPI from './api/ReadableAPI.js';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <Post />
-        <Post />
       </div>
     );
   }
---
+++ readable/src/api/ReadableAPI.js
@@ -0,0 +1,100 @@
+const api = "http://localhost:5001"
+
+let token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept' : 'application/json',
+  'Authorization' : token
+}
+
+export const getCategories = () =>
+    fetch(`${api}/categories`, { headers })
+    .then(res => res.json())
+
+export const getCategoryPosts = (category) =>
+    fetch(`${api}/${category}/posts`, {headers })
+    .then(res => res.json())
+
+export const getAllPosts = () =>
+    fetch(`${api}/posts`, {headers})
+    .then(res => res.json())
+
+export const addPost = (post) =>
+    fetch(`${api}/posts`,{
+      method: 'POST',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    }).then(res => res.json())
+
+export const getSinglePost = (id) =>
+    fetch(`${api}/posts/${id}`, { headers })
+    .then(res => res.json())
+
+export const addVoteToPost = (id, option) =>
+    fetch(`${api}/posts/${id}`,{
+      method: 'POST',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(option)
+    }).then(res => res.json())
+
+export const updatePost = (id, body) =>
+    fetch(`${api}/posts/${id}`,{
+      method: 'PUT',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    }).then(res => res.json())
+
+export const deletePost = (id) =>
+    fetch(`${api}/posts/${id}`,{
+      method: 'DELETE',
+      headers: {...headers}
+    }).then(res => res.json())
+
+export const getComments = (id) =>
+    fetch(`${api}/posts/${id}/comments`,{headers})
+      .then(res => res.json())
+
+export const addComment = (body) =>
+    fetch(`${api}/comments`,{
+      method: 'POST',
+      headers: {...headers,
+      'Content-Type': 'application/json'},
+      body: JSON.stringify(body)
+    }).then(res => res.json())
+
+export const getSingleComment = (id) =>
+    fetch(`${api}/comments/${id}`,{headers})
+    .then(res => res.json())
+
+export const addVoteToComment = (id, vote) =>
+    fetch(`${api}/comments/${id}`, {
+      method: 'POST',
+      headers: {...headers,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(vote)
+    }).then(res => res.json())
+
+export const updateComment = (id, body) =>
+    fetch(`${api}/comments/${id}`, {
+      method: 'PUT',
+      headers: {...headers,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    }).then(res => res.json())
+
+export const deleteComment = (id) =>
+    fetch(`${api}/comments/${id}`,{
+      method: 'DELETE',
+      headers: {...headers},
+    }).then(res => res.json())
